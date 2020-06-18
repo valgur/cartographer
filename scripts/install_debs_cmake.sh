07602ab35c19ccc3a66b86db6b50beb37c14fcc3
Wolfgang Hess <whess@lyft.com>
2020-06-18 15:44:58 +0300

    Add Ubuntu 20.04 to the install scripts, CI, docs. (#1706)
    Signed-off-by: Wolfgang Hess <whess@lyft.com>

--- scripts/install_debs_cmake.sh
+++ scripts/install_debs_cmake.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Copyright 2016 The Cartographer Authors
 #
@@ -19,17 +19,49 @@ set -o verbose
 
 # Install the required libraries that are available as debs.
 sudo apt-get update
+
+# Install CMake 3.2 for Debian Jessie.
+sudo apt-get install lsb-release -y
+if [[ "$(lsb_release -sc)" = "jessie" ]]
+then
+  sudo sh -c "echo 'deb [check-valid-until=no] http://archive.debian.org/debian jessie-backports main' >> /etc/apt/sources.list"
+  sudo sh -c "echo 'Acquire::Check-Valid-Until \"false\";' >> /etc/apt/apt.conf"
+  sudo apt-get update
+  sudo apt-get -t jessie-backports install cmake -y
+else
+  sudo apt-get install cmake -y
+fi
+
 sudo apt-get install -y \
-    cmake \
+    clang \
     g++ \
     git \
     google-mock \
     libboost-all-dev \
     libcairo2-dev \
+    libcurl4-openssl-dev \
     libeigen3-dev \
     libgflags-dev \
     libgoogle-glog-dev \
     liblua5.2-dev \
     libsuitesparse-dev \
-    ninja-build \
-    python-sphinx
+    ninja-build
+
+if [[ "$(lsb_release -sc)" = "focal" ]]
+then
+    sudo apt-get install -y python3-sphinx libgmock-dev
+else
+    sudo apt-get install -y python-sphinx
+fi
+
+# Install Ceres Solver on Ubuntu Bionic. No need to build it ourselves.
+if [[ "$(lsb_release -sc)" = "bionic" ]]
+then
+  sudo apt-get install -y libceres-dev
+fi
+
+# Install Ceres Solver and Protocol Buffers support on Ubuntu Focal. No need to build it ourselves.
+if [[ "$(lsb_release -sc)" = "focal" ]]
+then
+  sudo apt-get install -y libceres-dev protobuf-compiler
+fi
